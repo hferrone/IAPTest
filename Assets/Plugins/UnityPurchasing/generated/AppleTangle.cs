9c47dd8667717fb43fcd6ea432dc578e102ee06f
Harrison Ferrone <hferrone@gmail.com>
2019-06-03 18:58:39 -0600

    First project commit

---
+++ Assets/Plugins/UnityPurchasing/generated/AppleTangle.cs
@@ -0,0 +1,20 @@
+#if UNITY_ANDROID || UNITY_IPHONE || UNITY_STANDALONE_OSX || UNITY_TVOS
+// WARNING: Do not modify! Generated file.
+
+namespace UnityEngine.Purchasing.Security {
+    public class AppleTangle
+    {
+        private static byte[] data = System.Convert.FromBase64String("9+H119KE1drCzJChob208ZK0o6Xxvrfxpbm08aW5tL/xsKGhvbiysPUzOgBmoQ7elDD2GyC8qTw2ZMbGv7Xxsr6/tbiluL6/ovG+t/GkorReolCxF8qK2P5DYymVmSGx6U/EJAjnrhBWhAh2SGjjkyoJBKBPr3CD/5F3JpacrtmP4c7X0oTM8tXJ4cfV18LThILgwuHA19KE1dvC25ChodzX2PtXmVcm3NDQ1NTR0lPQ0NGNtl7ZZfEmGn398b6hZ+7Q4V1mkh7i54vhs+Da4djX0oTV18LThILgwri3uLKwpbi+v/GQpKW5vqO4pajgx+HF19KE1dLC3JChob208YO+vqVg4Yk9i9XjXbliXswPtKIuto+0bdY9rOhSWoLxAukVYG5Lntu6Lvot5OPg5eHi54vG3OLk4ePh6OPg5eEYyKMkjN8Ero5KI/TSa4RenIzcILXk8sSaxIjMYkUmJ01PHoFrEImBlK/OnbqBR5BYFaWz2sFSkFbiW1CzvbTxoqWwv7Wwo7XxpbSjvKLxsKvhU9Cn4d/X0oTM3tDQLtXV0tPQEbLipibr1v2HOgve8N8La6LInmT98bK0o6W4t7iysKW08aG+vbiyqK6QeUkoABu3TfW6wAFyajXK+xLOmAmnTuLFtHCmRRj809LQ0dByU9ChvbTxg76+pfGSkOHPxtzh5+Hl49fShMzf1cfVxfoBuJZFp9gvJbpcpbm+o7ilqODH4cXX0oTV0sLckKHhwNfShNXbwtuQoaG9tPGYv7L/4M5UUlTKSOyW5iN4SpFf/QVgQcMJiHbU2K3GkYfAz6UCZlry6pZyBL7+4VAS19n619DU1NbT0+FQZ8tQYnpyoEOWgoQQfv6QYikqMqEcN3KdRE+r3XWWWooFx+biGhXenB/FuACmpv+woaG9tP+yvrz+sKGhvbSysKjxsKKipLy0ovGwsrK0oaWwv7K08bC/tfGytKOluLe4srCluL6/8aFayFgPKJq9JNZ68+HTOcnvKYHYAqG9tPGStKOluLe4srCluL6/8ZCkZspsQpP1w/sW3sxnnE2PshmaUcbX4d7X0oTMwtDQLtXU4dLQ0C7hzN5M7CL6mPnLGS8fZGjfCI/NBxrsg7S9uLC/srTxvr/xpbm4ovGytKP7V5lXJtzQ0NTU0eGz4Nrh2NfShPGSkOFT0PPh3NfY+1eZVybc0NDQ2Y/hU9DA19KEzPHVU9DZ4VPQ1eGjsLKluLK08aKlsKW0vLS/paL/4VPQ0dfY+1eZVyaytdTQ4VAj4fvXbyWiSj8Dtd4aqJ7lCXPvKKkuuhlk63wl3t/RQ9pg8Mf/pQTt3Aqzx9n619DU1NbT0MfPuaWloaLr/v6meQ2v8+Qb9AQI3ge6BXP18sAmcH3nSJ38qWY8XUoNIqZKI6cDpuGeEOFT1WrhU9JycdLT0NPT0NPh3NfYvbTxmL+y/+D34fXX0oTV2sLMkKHs97bxW+K7JtxTHg86cv4ogruKtc5ACs+WgTrUPI+oVfw653OGnYQ9pbi3uLKwpbTxs6jxsL+o8aGwo6XU0dJT0N7R4VPQ29NT0NDRNUB42FHF+gG4lkWn2C8lulz/kXcmlpyugXtbBAs1LQHY1uZhpKTw");
+        private static int[] order = new int[] { 10,41,53,43,55,58,31,53,47,21,47,11,12,26,51,53,19,20,25,42,28,57,44,28,27,31,30,58,57,49,52,37,58,37,36,40,45,42,55,59,49,44,55,44,52,55,46,55,50,57,57,54,57,55,57,56,56,59,59,59,60 };
+        private static int key = 209;
+
+        public static readonly bool IsPopulated = true;
+
+        public static byte[] Data() {
+        	if (IsPopulated == false)
+        		return null;
+            return Obfuscator.DeObfuscate(data, order, key);
+        }
+    }
+}
+#endif
---
+++ Assets/Scripts/StoreManager.cs
@@ -0,0 +1,285 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Purchasing;
+using UnityEngine.Purchasing.Security;
+
+public class StoreManager : MonoBehaviour, IStoreListener
+{
+    // Setup variables
+    private static IStoreController _storeController;
+    private static IExtensionProvider _extensionProvider;
+
+    // Product IDs
+    public const string productIDConsumable_MediumPotion = "health_potion_medium";
+    public const string productIDNonConsumable_RareHelm = "gold_helm_rare";
+    public const string productIDSubscription_AutoRenew = "monthly_access_auto_renew";
+
+    // Computed properties
+    public bool isInitialized 
+    {
+        get { return _storeController != null && 
+                     _extensionProvider != null; }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitializeIAP();
+    }
+
+    #region Public IAP methods -> UI accessible
+    public void BuyConsumableItem()
+    {
+        PurchaseItem(productIDConsumable_MediumPotion);
+    }
+
+    public void BuyNonConsumableItem()
+    {
+        PurchaseItem(productIDNonConsumable_RareHelm);
+    }
+
+    public void RestorePurchases()
+    {
+        RestorePurchasedItems();
+    }
+
+    public void BuySubscription()
+    {
+        PurchaseItem(productIDSubscription_AutoRenew);
+    }
+    #endregion
+
+    #region Private IAP methods
+    private void InitializeIAP()
+    {
+        if(isInitialized)
+            return;
+
+        var purchasingModule = StandardPurchasingModule.Instance();
+        purchasingModule.useFakeStoreUIMode = FakeStoreUIMode.DeveloperUser;
+
+        var builder = ConfigurationBuilder.Instance(purchasingModule);
+
+        builder.AddProduct(productIDConsumable_MediumPotion, ProductType.Consumable);
+        builder.AddProduct(productIDNonConsumable_RareHelm, ProductType.NonConsumable, null, new PayoutDefinition(PayoutType.Item, "nothing", 1, "payout data"));
+
+        builder.AddProduct(productIDSubscription_AutoRenew, ProductType.Subscription, new IDs
+        {
+            {"com.merchantmercenaries.subscription.auto", MacAppStore.Name},
+            {"come.merchantmercenaries.subscription.automatic", GooglePlay.Name}
+        });
+
+        UnityPurchasing.Initialize(this, builder);
+    }
+
+    private void PurchaseItem(string productID)
+    {
+        if(!isInitialized)
+        {
+            Debug.Log("Product purchase failed, IAP not initialized...");
+            return;
+        }
+
+        Product currentProduct = _storeController.products.WithID(productID);
+        if(currentProduct != null && currentProduct.availableToPurchase)
+        {
+            _storeController.InitiatePurchase(currentProduct);
+            Debug.LogFormat("Attempting to purchase item {0} asynchronously", currentProduct.definition.id);
+        }
+        else 
+        {
+           Debug.LogFormat("Attempt to purchase item {0} failed - item was not found or unavailable...", currentProduct.definition.id);
+        }
+    }
+
+    private void RestorePurchasedItems()
+    {
+        if(!isInitialized)
+        {
+            Debug.Log("Purchase restoration failed, IAP not initialized!");
+            return;
+        }
+
+        if(Application.platform == RuntimePlatform.IPhonePlayer || 
+           Application.platform == RuntimePlatform.OSXPlayer || 
+           Application.platform == RuntimePlatform.tvOS)
+        {
+            var appleExtension = _extensionProvider.GetExtension<IAppleExtensions>();
+            appleExtension.RestoreTransactions((restoreResult) => {
+                Debug.LogFormat("Purchase restoration processing: {0}. If ProcessPurchase doesn't fire there are no products to restore.", restoreResult);
+            });
+        }
+        else if(Application.platform == RuntimePlatform.Android ||Application.platform == RuntimePlatform.WindowsPlayer)
+        {
+            Debug.Log("Purchases have been restored automatically, and ProcessPurchase will fire for every restored item found!");
+        }
+        else
+        {
+            Debug.LogFormat("Purchase restoration not supported on {0} platform", Application.platform);
+        }
+    }
+
+    private void QuerySubscriptionInfo(Product product)
+    {
+        Dictionary<string, string> intro_price_dict = _extensionProvider.GetExtension<IAppleExtensions>().GetIntroductoryPriceDictionary();
+
+        if (product.receipt != null)
+        {
+            Debug.Log(product.receipt);
+
+            if (product.definition.type == ProductType.Subscription)
+            {
+                string price_json = (intro_price_dict == null || !intro_price_dict.ContainsKey(product.definition.storeSpecificId)) ? null : intro_price_dict[product.definition.storeSpecificId];
+
+                SubscriptionManager p = new SubscriptionManager(product, price_json);
+                SubscriptionInfo info = p.getSubscriptionInfo();
+
+                Debug.LogFormat("{0}, {1}, {2}", info.getPurchaseDate(), info.getExpireDate(), info.isAutoRenewing());
+            }
+            else
+            {
+                Debug.Log("This product is not a subscription...");
+            }
+        }
+        else
+        {
+            Debug.Log("This product does not have a valid receipt...");
+        }
+    }
+
+    private bool ValidateReceipt(Product product)
+    {
+        bool validReceipt = true;
+
+#if UNITY_IOS || UNITY_ANDROID || UNITY_STANDALONE_OSX
+        var validator = new CrossPlatformValidator(AppleTangle.Data(), GooglePlayTangle.Data(), Application.identifier);
+
+        try
+        {
+            var result = validator.Validate(product.receipt);
+            foreach (IPurchaseReceipt productReceipt in result)
+            {
+                Debug.LogFormat("{0}, {1}, {2}", productReceipt.productID, 
+                                                 productReceipt.purchaseDate, 
+                                                 productReceipt.transactionID);
+
+                AppleInAppPurchaseReceipt aReceipt = productReceipt as AppleInAppPurchaseReceipt;
+                if (aReceipt != null)
+                {
+                    Debug.LogFormat("{0}, {1}, {2}, {3}", aReceipt.originalTransactionIdentifier,
+                                                          aReceipt.subscriptionExpirationDate,
+                                                          aReceipt.cancellationDate,
+                                                          aReceipt.quantity);
+                }
+
+                GooglePlayReceipt gReceipt = productReceipt as GooglePlayReceipt;
+                if (gReceipt != null)
+                {
+                    Debug.LogFormat("{0}, {1}, {2}", gReceipt.transactionID, 
+                                                     gReceipt.purchaseState, 
+                                                     gReceipt.purchaseToken);
+                }
+            }
+        }
+        catch (MissingStoreSecretException)
+        {
+            Debug.Log("You haven't supplied a secret key for this platform...");
+            validReceipt = false;
+        }
+        catch (IAPSecurityException)
+        {
+            Debug.LogFormat("Invalid receipt {0}", product.receipt);
+            validReceipt = false;
+        }
+#endif
+
+        return validReceipt;
+    }
+    #endregion
+
+    #region IStoreListener methods
+    public void OnInitialized(IStoreController controller, IExtensionProvider extensions)
+    {
+        _storeController = controller;
+        _extensionProvider = extensions;
+
+        Debug.Log("IAP initialized!");
+
+        _extensionProvider.GetExtension<IAppleExtensions>().RegisterPurchaseDeferredListener((item) => {
+            Debug.LogFormat("Transaction deferred for {0}", item.definition.id);
+        });
+
+        foreach (var product in controller.products.all) 
+        {
+            Debug.Log (product.metadata.localizedTitle);
+            Debug.Log (product.metadata.localizedDescription);
+            Debug.Log (product.metadata.localizedPriceString);
+        }
+    }
+
+    public void OnInitializeFailed(InitializationFailureReason error)
+    {
+        Debug.Log("Purchasing failed to initialize...");
+        switch (error)
+        {
+            case InitializationFailureReason.AppNotKnown:
+                Debug.LogError("Check your App is correctly configured on your publishing platform...");
+                break;
+            case InitializationFailureReason.PurchasingUnavailable:
+                Debug.Log("Purchasing service is unavailable...");
+                break;
+            case InitializationFailureReason.NoProductsAvailable:
+                Debug.Log("No products are available for purchase...");
+                break;
+        }
+    }
+
+    public PurchaseProcessingResult ProcessPurchase(PurchaseEventArgs args)
+    {
+        string productID = args.purchasedProduct.definition.id;
+
+        PayoutDefinition payout = args.purchasedProduct.definition.payout;
+        if (payout != null)
+            Debug.Log("Payout for this item detected...");
+
+        bool validReceipt = ValidateReceipt(args.purchasedProduct);
+
+        switch (productID)
+        {
+            case productIDConsumable_MediumPotion:
+                Debug.LogFormat("Consumable product {0} successfully purchased!", productID);
+                break;
+            case productIDNonConsumable_RareHelm:
+                Debug.LogFormat("Non-Consumable product {0} successfully purchased!", productID);
+                break;
+            case productIDSubscription_AutoRenew:
+                Debug.LogFormat("Subscription {0} successfully purchased!", productID);
+                QuerySubscriptionInfo(args.purchasedProduct);
+                break;
+            default:
+                Debug.LogFormat("Product purchase failed due to unrecognized product ID -> {0}", productID);
+                break;
+        }
+
+        return PurchaseProcessingResult.Complete;
+    }
+
+    public void OnPurchaseFailed(Product product, PurchaseFailureReason failureReason)
+    {
+        Debug.LogFormat(string.Format("Product {0} purchase failed...", product.definition.storeSpecificId));
+        switch(failureReason)
+        {
+            case PurchaseFailureReason.PaymentDeclined:
+                Debug.Log("Your payment was declined...");
+                break;
+            case PurchaseFailureReason.ProductUnavailable:
+                Debug.Log("That product is no longer available...");
+                break;
+            case PurchaseFailureReason.Unknown:
+                Debug.Log("An unknown problem occurred with your purchase...");
+                break;
+        }
+    }
+    #endregion
+}
